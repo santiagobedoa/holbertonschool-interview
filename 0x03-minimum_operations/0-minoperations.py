45a42ac0abb2e564133e1ce95128cb0dd4c984fb
santiagobedoa <santiagobedoa@gmail.com>
2023-01-05 19:36:53 -0500

    Thu Jan  5 19:36:53 -05 2023

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -7,24 +7,54 @@ of operations needed to result in exactly n H characters in the file.
 """
 
 
-def countProcess(num):
-    """Return list of process until n H"""
-    con = 1
-    p_list = []
-    val = num
-    while val != 1:
-        con += 1
-        if val % con == 0:
-            while val % con == 0 and val != 1:
-                val /= con
-                p_list.append(con)
+def countProcess(num: int) -> list[int]:
+    """
+    Returns a list of the prime factors of num.
 
-    return p_list
+    Parameters:
+    - num: An integer for which the prime factors will be calculated.
+
+    Returns:
+    - A list of the prime factors of num.
+    """
+    # Initialize a counter variable to 1
+    counter = 1
+    # Initialize an empty list to store the prime factors
+    primeList = []
+    # Initialize a variable to store the target num
+    target = num
+    # Loop until target is 1
+    while target != 1:
+        # Increment counter by 1
+        counter += 1
+        # If target is divisible by counter, enter the inner loop
+        if target % counter == 0:
+            while target % counter == 0 and target != 1:
+                # Divide target by counter and append counter to the list of prime factors
+                target /= counter
+                primeList.append(counter)
+    # Return the list of prime factors
+    return primeList
 
 
-def minOperations(n):
-    """Return sum of process until n H"""
+def minOperations(n: int) -> int:
+    """
+    Calculates the fewest number of operations needed to result in exactly n H
+    characters in a file using a text editor that can execute only two
+    operations: Copy All and Paste.
+
+    Parameters:
+    - n: An integer representing the target number of H characters.
+
+    Returns:
+    - An integer representing the minimum number of operations needed to
+    get to n H characters.
+    - If it is not possible to get to n H characters, returns 0.
+    """
+    # If n is less than 2 or not an integer, return 0
     if n < 2 or type(n) is not int:
         return 0
+    # Get the list of prime factors of n
     values = countProcess(n)
+    # Return the sum of the elements in the list of prime factors
     return sum(values)
