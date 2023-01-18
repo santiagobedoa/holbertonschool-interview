8ec0cdcd6955941f2d7f88352160972300bbc8b6
santiagobedoa <santiagobedoa@gmail.com>
2023-01-18 11:19:54 -0500

    Wed Jan 18 11:19:54 -05 2023

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -1,62 +1,63 @@
 #include "lists.h"
 
 /**
+ * reverse_linked_list - function that reverse a linked list
+ * @head: head of the linked list
+ * Return: void
+ */
+void reverse_linked_list(listint_t **head)
+{
+	listint_t *prev = NULL;
+	listint_t *current = *head;
+	listint_t *next = NULL;
+
+	while (current)
+	{
+		next = current->next;
+        	current->next = prev;
+        	prev = current;
+        	current = next;
+    	}
+
+    	*head = prev;
+}
+
+/**
  * is_palindrome - checks if a linked list is palindrome
  * @head: head of the linked list
  * Return: 1 if the linked list is palindrome, 0 otherwise
  */
-int is_palindrome(listint_t **head) {
-  listint_t *hare = *head;
-  listint_t *tortoise = *head;
-  listint_t *first_half = *head;
-  listint_t *second_half;
+int is_palindrome(listint_t **head)
+{
+	listint_t *copy_1 = *head;
+	listint_t *copy_2 = *head;
+	listint_t *copy_3 = *head;
+	int num_of_nodes = 0;
+	int half = 0;
 
-  if (*head == NULL)
-    return (1);
-
-  while (1) {
-    hare = hare->next->next;
-    if (hare == NULL) {
-      second_half = tortoise->next;
-      break;
-    }
-    if (hare->next == NULL) {
-      second_half = tortoise->next->next;
-      break;
-    }
-
-    tortoise = tortoise->next;
-  }
-
-  reverse_linked_list(&second_half);
-
-  while (first_half && second_half) {
-    if (first_half->n != second_half->n) {
-      return (0);
-    }
-    first_half = first_half->next;
-    second_half = second_half->next;
-  }
-  return (1);
-}
-
-/**
- * reverse_linked_list - function that reverse a linked list
- * @head: double pointer to the head of the linked list 
- * Return: pointer to the head of the reversed linked list
- */
-void reverse_linked_list(listint_t **head) {
-  listint_t *current = *head;
-  listint_t *prev = NULL;
-  listint_t *next = NULL;
-
-  printf("reverse_linked_list");
-  while (current != NULL) {
-    next = current->next;
-    current->next = prev;
-    prev = current;
-    current = next;
-  }
-
-  *head = prev;
+	if (*head == NULL)
+	{
+		return (1);
+	}
+	while (copy_1)
+	{
+		num_of_nodes += 1;
+		copy_1 = copy_1->next;
+	}
+	while (half != (num_of_nodes / 2))
+	{
+		half += 1;
+		copy_2 = copy_2->next;
+	}
+	reverse_linked_list(&copy_2);
+	while (copy_3 && copy_2)
+	{
+		if (copy_3->n != copy_2->n)
+		{
+			return (0);
+		}
+		copy_3 = copy_3->next;
+		copy_2 = copy_2->next;
+	}
+	return (1);
 }
