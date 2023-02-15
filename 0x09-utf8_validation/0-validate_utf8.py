c2f479fa173f228554899e5c28699be019a5ff7e
santiagobedoa <santiagobedoa@gmail.com>
2023-02-15 11:20:55 -0500

    Wed Feb 15 11:20:55 -05 2023

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -10,30 +10,38 @@ def validUTF8(data):
     data represented by a list of integers to check
     """
 
-    bit1 = 1 << 7
-    bit2 = 1 << 6
+    # Define the bit masks and counters
+    bit1 = 1 << 7  # 0b10000000
+    bit2 = 1 << 6  # 0b01000000
     nbytes = 0
 
+    # If the data is empty, return True
     if not data or len(data) == 0:
         return True
 
+    # Iterate over the data and check each byte
     for num in data:
         bit = 1 << 7
         if nbytes == 0:
+            # Check how many bytes there are in the current sequence
             while bit & num:
                 nbytes += 1
                 bit = bit >> 1
 
+            # If there are no bytes or too many, return False
             if nbytes == 0:
                 continue
             if nbytes == 1 or nbytes > 4:
                 return False
         else:
-
+            # If this is not the first byte in the sequence, check if it has the right bit pattern
             if not (num & bit1 and not (num & bit2)):
                 return False
+
+        # Decrement the byte counter
         nbytes -= 1
 
+    # If the counter is not zero at the end, return False
     if nbytes:
         return False
     else:
