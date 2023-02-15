e59e35a93468f5af7472c8adf80417fb33c0d855
santiagobedoa <santiagobedoa@gmail.com>
2023-02-15 11:15:35 -0500

    Wed Feb 15 11:15:35 -05 2023

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -9,34 +9,32 @@ def validUTF8(data):
     nbytes keeps track of how many 1s before 0 occurs
     data represented by a list of integers to check
     """
-    # Count the number of leading 1 bits in the first byte of the current char
-    num_bytes = 0
-    for i in range(8):
-        if data[0] & (1 << (7 - i)) == 0:
-            break
-        num_bytes += 1
 
-    # Check that the remaining bytes are of the form 10xxxxxx
-    if num_bytes > 1:
-        for j in range(1, num_bytes):
-            if j >= len(data) or (data[j] & 0b11000000) != 0b10000000:
+    bit1 = 1 << 7
+    bit2 = 1 << 6
+    nbytes = 0
+
+    if not data or len(data) == 0:
+        return True
+
+    for num in data:
+        bit = 1 << 7
+        if nbytes == 0:
+            while bit & num:
+                nbytes += 1
+                bit = bit >> 1
+
+            if nbytes == 0:
+                continue
+            if nbytes == 1 or nbytes > 4:
                 return False
+        else:
 
-    # Check that the first byte is of the form:
-    # 0xxxxxxx, 110xxxxx, 1110xxxx, or 11110xxx
-    if num_bytes == 0:
-        if data[0] & 0b10000000 != 0:
-            return False
-    elif num_bytes == 1:
+            if not (num & bit1 and not (num & bit2)):
+                return False
+        nbytes -= 1
+
+    if nbytes:
         return False
-    elif num_bytes == 2:
-        if data[0] & 0b11100000 != 0b11000000:
-            return False
-    elif num_bytes == 3:
-        if data[0] & 0b11110000 != 0b11100000:
-            return False
-    elif num_bytes == 4:
-        if data[0] & 0b11111000 != 0b11110000:
-            return False
-
-    return True
+    else:
+        return True
