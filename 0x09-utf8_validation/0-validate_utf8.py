e0a48cc3edb2ba78164781dde786962423fcb987
santiagobedoa <santiagobedoa@gmail.com>
2023-02-15 11:12:51 -0500

    Wed Feb 15 11:12:51 -05 2023

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Determines a valid UTF-8 encoding"""
+
+
+def validUTF8(data):
+    # Count the number of leading 1 bits in the first byte of the current char
+    num_bytes = 0
+    for i in range(8):
+        if data[0] & (1 << (7 - i)) == 0:
+            break
+        num_bytes += 1
+
+    # Check that the remaining bytes are of the form 10xxxxxx
+    if num_bytes > 1:
+        for j in range(1, num_bytes):
+            if j >= len(data) or (data[j] & 0b11000000) != 0b10000000:
+                return False
+
+    # Check that the first byte is of the form:
+    # 0xxxxxxx, 110xxxxx, 1110xxxx, or 11110xxx
+    if num_bytes == 0:
+        if data[0] & 0b10000000 != 0:
+            return False
+    elif num_bytes == 1:
+        return False
+    elif num_bytes == 2:
+        if data[0] & 0b11100000 != 0b11000000:
+            return False
+    elif num_bytes == 3:
+        if data[0] & 0b11110000 != 0b11100000:
+            return False
+    elif num_bytes == 4:
+        if data[0] & 0b11111000 != 0b11110000:
+            return False
+
+    return True
